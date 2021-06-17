458511855a36fc46236024ad63ec9be368831e29
dean <deanraguso@gmail.com>
2021-06-17 14:52:23 +1000

    Created: Base grid with individual nodes.

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -2,16 +2,40 @@ import React, {Component} from 'react';
 
 import './PathfindingVisualizer.css';
 
+import Node from './Node/Node';
+
 export default class PathfindingVisualizer extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            nodes: [],
+        };
+    }
+
+    componentDidMount(){
+        const nodes = [];
+        for (let row = 0; row < 20; row++){
+            const currentRow = [];
+            for(let col = 0; col < 50; col++){
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes})
     }
 
     render() {
+        const {nodes} = this.state;
+
         return (
-            <div>
-                Foo
+            <div className="grid">
+                {nodes.map((row, rowIdx) => {
+
+                  return <div className="row">
+                            {row.map((node, nodeIdx) => <Node></Node>)}
+                        </div>
+                        
+                })}
             </div>
         )
     }
